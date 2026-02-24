18bb856f920b72c8c685daba9a22589a894a8940
Leinierb <breindelmarcial@gmail.com>
2026-02-24 15:01:11 +0400

    first commit

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,12 @@
-greeting = input("Hello, possible pirate! What's the password?)
+greeting = input("Hello, possible pirate! What's the password?")
 if greeting in ["Arrr!"]: # NG replaced ) by ]
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
+
+"""Errors
+Missing " in line 1
+Wrong indentation in line 3
+Invalid elif in line 4
+Missing indentation in line5
+"""
