988cc7d9f1f80bb8d81a37786d998a76c4edce26
Leinierb <breindelmarcial@gmail.com>
2026-02-26 15:37:06 +0400

    Fixed UnfinishedCowbull

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -2,12 +2,34 @@ import random
 
 def compare_numbers(number, user_guess):
     ## your code here
+    Cow = 0
+    Bull = 0
+    
+    if len(user_guess) < 4:
+        for i in range(4 - len(user_guess)):
+            user_guess = "0"+user_guess
+
+    if len(number) < 4:
+        for i in range(4 - len(number)):
+            number = "0"+number
+            
+    for n in number:
+        if n in user_guess:
+            Cow += 1
+
+    for n in range(len(number)):
+        if number[n] == user_guess[n]:
+            Bull += 1
+            Cow -= 1
+
+    cowbull = [Cow, Bull]
+    
     return cowbull
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+print (number)
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +38,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!")
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
